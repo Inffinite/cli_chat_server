562d2539c8fe3d2150b0413825c3576c2c6c607a
clown <clown@localhost.localdomain>
2020-04-22 02:15:07 +0300

    :hammer: Init commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const socket = require('socket.io')
+const express = require('express')
+const cors = require('cors')
+const chalk = require('chalk')
+const emoji = require('node-emoji')
+
+const app = express()
+const port = process.env.PORT 
+
+app.use(cors())
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+    next()
+})
+
+const server = app.listen(port, () => {
+    console.log(chalk.yellow('[+]' + emoji.get('pizza') + ' The server is up on port ' + port))
+})
+
+let io = socket(server)
+
+io.set('origins', '*:*')
+io.on("connection", (socket) => {
+    // console.log('New connection established: ' + socket.id)
+    const data = '[****' + emoji.get('pizza') + '++++]' + ' Welcome to the edge of the internet. Niaje buda!'
+    io.emit('Fuckery', data)
+
+    socket.on('newMessage', (data) => {
+        socket.broadcast.emit('incomingMessage', data )
+    })
+
+    io.on('disconnect', () => {
+        const data = emoji.get('tophat') + ' Your chat buddy went offline.'
+        socket.broadcast.emit('disconnect', data)
+    })
+})
